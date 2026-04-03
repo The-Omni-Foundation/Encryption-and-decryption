640bba403ac3560568adf69e3801e3a94ff78f24
leifbraychar <152452841+leifbraychar@users.noreply.github.com>
2026-04-03 03:16:40 -0400

    Update Decryptor.py
    Added filedialog from tikinter
    
    bunch of other optimization

--- Decryptor.py
+++ Decryptor.py
@@ -1,53 +1,84 @@
 import os
 from cryptography.fernet import Fernet
 from pathlib import Path
+import tkinter
+from tkinter import filedialog
 
 def decrypt_items(target_path, key):
-    with open(target_path, "rb") as file:
-        encrypted_data = file.read()
+    try:
+        with open(target_path, "rb") as file:
+                encrypted_data = file.read()
 
-    decrypted_data = key.decrypt(encrypted_data)
+        if not encrypted_data.startswith(b'gAAAAA'):
+            return
 
-    with open(target_path, "wb") as file:
-        file.write(decrypted_data)
+        decrypted_data = key.decrypt(encrypted_data)
 
+        with open(target_path, "wb") as file:
+                file.write(decrypted_data)
+        print("Success!")
+    except Exception as e:
+            print(f"Error decrypting {os.path.basename(target_path)}: {type(e).__name__} - {e}")
 
-#loading existing key
-key_raw = input("Enter name of key (don't include .key) ")
+def find_file_path():
+    root = tkinter.Tk()
+    root.withdraw()
+    path = filedialog.askopenfilename()
+    root.destroy()
+    return path
+     
+def find_dir_path():
+    root = tkinter.Tk()
+    root.withdraw()
+    path = filedialog.askdirectory()
+    root.destroy()
+    return path
 
-if not key_raw.lower().endswith(".key"):
-    key_name = key_raw + ".key"
-else:
-    key_name = key_raw
+while True:
 
-try: 
-    with open(key_name, "rb") as key_file:
-        key = key_file.read()
+        #loading existing key
+    key_raw = input("Enter name of key (don't include .key) ")
 
-    fernet_key = Fernet(key)
+    if not key_raw.lower().endswith(".key"):
+        key_name = key_raw + ".key"
+    else:
+        key_name = key_raw
 
-except Exception as e:
-    print(f"Could not decrypt: {e}")
+    key_path = Path(key_raw).with_suffix(".key")
 
-#get path
-path = input("Enter path to encrypted items ")
+    if key_path.is_file():
+        print("Found!")
 
-if os.path.isfile(path):
+    try: 
+        with open(key_name, "rb") as key_file:
+            key = key_file.read()
 
-    decrypt_items(path, fernet_key)
+        fernet_key = Fernet(key)
 
-elif os.path.isdir(path):
+    except Exception as e:
+        print(f"Could not decrypt: {e}")
 
-    for root, dirs, files in os.walk(path):
+    typeofpath = input("file or directory? (f/d) ")
 
-        for filename in files:
-            if filename == key_name:
-                continue
+    if typeofpath.lower() in ['d', "directory"]:
+            path = find_dir_path()
 
-            full_path = os.path.join(root, filename)
-            
-            try:
+    elif typeofpath.lower() in ['f', "file"]:
+            path = find_file_path()
+
+    if os.path.isfile(path):
+
+        decrypt_items(path, fernet_key)
+
+    elif os.path.isdir(path):
+
+        for root, dirs, files in os.walk(path):
+
+            for filename in files:
+                if filename == key_path.name:
+                    continue
+
+                full_path = os.path.join(root, filename)
                 decrypt_items(full_path, fernet_key)
 
-            except Exception as e:
-                print(f"Could not decrypt: {e}")
+                
